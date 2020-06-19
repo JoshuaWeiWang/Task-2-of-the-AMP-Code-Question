f963ff325174de75fc4b1cb33f419bbb2af38f77
JoshuaWeiWang <joshuaweiwang@gmail.com>
2020-06-19 12:00:17 +1000

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,22 +1,23 @@
 import re
 
-# Enter the values of N and M, respectively
-DIM = input('Please enter the values of N and M: ').strip().split()
-N, M = int(DIM[0]), int(DIM[1])
+if __name__ == '__main__':
+    # Enter the values of N and M, respectively
+    DIM = input('Please enter the values of N and M: ').strip().split()
+    N, M = int(DIM[0]), int(DIM[1])
 
-# Initialize the input matrix
-matrix = []
+    # Initialize the input matrix
+    matrix = []
 
-for row in range(N):
-    matrix.append(input('Please enter the items in No. {row} row: '
-                        .format(row = row + 1)))
+    for row in range(N):
+        matrix.append(input('Please enter the items in No. {row} row: '
+                            .format(row = row + 1)))
 
-# decode the matrix script as output
-output = "".join([row[item] for item in range(M) for row in matrix])
+    # decode the matrix script as output
+    output = "".join([row[item] for item in range(M) for row in matrix])
 
-print('The decoded script is: {output}'.format(output = output))
+    print('The decoded script is: {output}'.format(output = output))
 
-output = re.sub(r'\b[^a-zA-Z0-9]+\b', ' ', output)
-output = re.sub('  ', ' ', output)
+    output = re.sub(r'\b[^a-zA-Z0-9]+\b', ' ', output)
+    output = re.sub('  ', ' ', output)
 
-print('The final decoded script is: {output}'.format(output = output))
+    print('The final decoded script is: {output}'.format(output = output))
